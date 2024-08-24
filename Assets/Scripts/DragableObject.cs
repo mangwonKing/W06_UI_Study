3b602dacf9a63438c071580797d363f5721ba46d
Kim_EuiMyoung <aud1035@naver.com>
2024-08-24 10:51:42 +0900

    마우스 위치에 대응되도록 dragable object 수정

--- Assets/Scripts/DragableObject.cs
+++ Assets/Scripts/DragableObject.cs
@@ -12,19 +12,21 @@ public class DragableObject : MonoBehaviour, IBeginDragHandler, IDragHandler, IE
     private Vector2 originalPosition;
     private Canvas canvas;
 
+    Vector2 offset;
     private void Awake()
     {
         rectTransform = GetComponent<RectTransform>();
         canvas = GetComponentInParent<Canvas>();
-        originalPosition = rectTransform.anchoredPosition;
+        //originalPosition = rectTransform.anchoredPosition;
     }
     public void OnBeginDrag(PointerEventData eventData)
     {
         print("드래그");
+        offset = (Vector2)rectTransform.position - eventData.position;
     }
     public void OnDrag(PointerEventData eventData)
     {
-        rectTransform.anchoredPosition = eventData.position;
+        rectTransform.position = eventData.position + offset;
     }
 
     public void OnEndDrag(PointerEventData eventData)
@@ -35,8 +37,8 @@ public class DragableObject : MonoBehaviour, IBeginDragHandler, IDragHandler, IE
             Debug.Log("Dropped on the panel!");
             // 패널 위에 놓여진 경우의 처리
             changeObject.SetActive(true);
-            //changeObject.transform.position = eventData.position;
-            changeObject.GetComponent<RectTransform>().anchoredPosition = eventData.position;
+            changeObject.transform.position = eventData.position;
+            //changeObject.GetComponent<RectTransform>().anchoredPosition = eventData.position;
             eventData.pointerDrag.SetActive(false);
         }
         else
